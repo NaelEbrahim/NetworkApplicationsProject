4f23a1adb2a6327565f6371be75b507e3485aa6e
Nael Ebrahim <naelebrahim@gmail.com>
2024-12-27 17:26:59 +0300

    update: Update CORS policy to allow localhost:8989 to access the API

--- src/main/java/NetworkApplicationsProject/Config/SecurityConfig.java
+++ src/main/java/NetworkApplicationsProject/Config/SecurityConfig.java
@@ -2,12 +2,17 @@ package NetworkApplicationsProject.Config;
 
 import NetworkApplicationsProject.Security.JWTRequestFilter;
 import lombok.AllArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @AllArgsConstructor
@@ -17,7 +22,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:8989")); // Allow all origins
+        configuration.setAllowedMethods(List.of("*")); // Allow all HTTP methods
+        configuration.setAllowedHeaders(List.of("*")); // Allow all headers
+        configuration.setAllowCredentials(false); // Adjust based on your requirements
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        httpSecurity.cors(cors -> cors.configurationSource(source)).csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("api/auth/register","api/auth/login", "/swagger-ui/**",
